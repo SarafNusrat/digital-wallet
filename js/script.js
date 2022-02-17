2041dc4ef80acb6edd0c4c37bca054d075538a06
SarafNusrat <saraf.nusrat11@gmail.com>
2022-02-17 18:03:53 +0600

    comments left

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
 function calculateButton(){
-    debugger;
     const income = getInputValue("income");
     const totalExpense = addAllExpense();
+    console.log(totalExpense);
+    if (totalExpense == 0) { 
+        return;
+    }
     const totalBalance = income - totalExpense;
     const expences = document.getElementById("total-expense");
     expences.innerText = totalExpense;
-    const balance = document.getElementById("balance");
-    balance.innerText = totalBalance;
+    if (totalBalance < 0) {
+        const expensesMessage = document.getElementById("notify-expenses");
+        expensesMessage.style.display = "block";
+    }
+    else {
+        const balance = document.getElementById("balance");
+        balance.innerText = totalBalance;  
+    }
 }
 
 function getInputValue(inputId){
     const inputField = document.getElementById(inputId);
      const inputAmountText = inputField.value;
      const amountValue = parseFloat(inputAmountText);
+     verifyInput(amountValue);
      return amountValue;
 }
 
@@ -20,8 +30,13 @@ function addAllExpense() {
     const foodExp = getInputValue("food-exp");
     const rentExp = getInputValue("rent-exp");
     const clothesExp = getInputValue("clothes-exp");
-    const totalExpense = foodExp + rentExp + clothesExp;
-    return totalExpense;
+    if (foodExp < 0 || rentExp < 0 || clothesExp < 0) {
+        return 0;
+    }
+    else {
+        const totalExpense = foodExp + rentExp + clothesExp;
+        return totalExpense;
+    }
 }
 
 function saveButton(){
@@ -31,6 +46,10 @@ function saveButton(){
     const savePercentage = getInputValue("save-percentage");
     // calculate savings 
     const savingAmount = document.getElementById("saving-amount");
+    if (savePercentage < 0) 
+    {
+        return;
+    }
     const calculatedSavings = income * (savePercentage/100); 
     savingAmount.innerText = calculatedSavings;
     // get balance
@@ -49,86 +68,17 @@ function saveButton(){
 }
 
 
-
-// function updateTotalField(totalFieldId, depositAmount){
-//     const totalElement = document.getElementById(totalFieldId);
-//      const totalText = totalElement.innerText;
-//      const previousTotal = parseFloat(totalText);
-
-//      totalElement.innerText = previousTotal + depositAmount; 
-// }
-
-// function getCurrentBalance() {
-//     const balanceTotal= document.getElementById('balance-total');
-//     const balanceTotalText = balanceTotal.innerText;
-//     const previousBalanceTotal = parseFloat(balanceTotalText);
-//     return previousBalanceTotal;
-// }
-
-// function updateBalance(amount, isAdd){
-//     const balanceTotal= document.getElementById('balance-total');
-//     const previousBalanceTotal = getCurrentBalance();
-//     if(isAdd == true) {
-//         balanceTotal.innerText = previousBalanceTotal + amount;
-//     }
-//     else {
-//         balanceTotal.innerText = previousBalanceTotal - amount;
-//     }  
-// }
-
-
-// document.getElementById('deposit-button').addEventListener('click', function()
-// {
-//     const depositAmount = getInputValue('deposit-input');
-//     if (depositAmount > 0)
-//     {
-//         updateTotalField('deposit-total',depositAmount);
-//         updateBalance(depositAmount, true);
-//     }
-// })
-
-// document.getElementById('withdraw-button').addEventListener('click', function()
-// {
-//      const withdrawAmount = getInputValue('withdraw-input');
-//      const currenBalance = getCurrentBalance();
-//      if (withdrawAmount > 0 && withdrawAmount < currenBalance)
-//      {
-//         updateTotalField('withdraw-total',withdrawAmount);
-//         updateBalance(withdrawAmount, false);
-//      }
-//      if (withdrawAmount > currenBalance)
-//      {
-//         console.log('You can not withdraw more than what you have in your account.');
-//      }
-// })
-
-
-/* 
-
-function verifyPin() {
+function verifyInput(amountValue) {
     debugger;
-    const pin = document.getElementById("display-pin").value;
-    const typedNumbers = document
-    .getElementById("typed-numbers").value;
-    const successMessage = document.getElementById("notify-success");
-    const failError = document.getElementById("notify-fail");
-    if (pin == typedNumbers) {
-        successMessage.style.display = "block";
-        failError.style.display = "none"
+
+    if (amountValue >= 0) {
+        return amountValue;
     }
     else {
-        successMessage.style.display = "none";
-        failError.style.display = "block";
+        const negStringMessage = document.getElementById("notify-negString");
+        negStringMessage.style.display = "block";
+        return;
     }
 }
- */
 
-// if (withdrawAmount > 0 && withdrawAmount < currenBalance)
-// {
-//    updateTotalField('withdraw-total',withdrawAmount);
-//    updateBalance(withdrawAmount, false);
-// }
-// if (withdrawAmount > currenBalance)
-// {
-//    console.log('You can not withdraw more than what you have in your account.');
-// }
+
